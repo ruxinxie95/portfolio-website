b7e2907878afe9740e5ca982f223fcfd498b62d2
Ruxin Xie <ruxinx.@umich.edu>
2024-09-18 11:27:06 -0400

    website before integrating Hugo

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
             // Create the article element with a link to the individual project page
             var article = `
                 <article class="project ${categoryClasses}">
-                    <a href="projects/project${index + 1}.html" class="project-link">
+                    <a href="projects/project${index + 1}/project${index + 1}.html" class="project-link">
                         <img src="${project.cover_image}" alt="${project.title}" class="wp-post-image">
                     </a>
                 </article>
@@ -72,29 +72,4 @@ $(document).ready(function() {
             }
         });
     });
-
-    // Slideshow functionality for project pages
-    if ($('.slideshow').length > 0) {
-        $('.slideshow').slick({
-            arrows: true,
-            dots: true,
-            autoplay: false,  // Disable auto-sliding
-            infinite: false,
-            centerMode: true,
-            centerPadding: '33.33%', // Slideshow starts 1/3 from the right and ends 1/3 from the left
-            slidesToShow: 1
-        });
-
-        // Lightbox trigger for images in the slideshow
-        $('.slideshow img').on('click', function(e) {
-            e.preventDefault();
-            var lightboxSrc = $(this).attr('src');
-            lightbox.start($(this)[0]); // Manually trigger lightbox with image clicked
-        });
-
-        // Lightbox close when clicked outside of the image
-        $(document).on('click', '.lightbox', function() {
-            lightbox.end();
-        });
-    }
 });
