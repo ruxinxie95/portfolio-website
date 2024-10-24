d8a90ab4c6b611e47b1fe39ac7e4df332fb7f32c
Ruxin Xie <ruxinx.@umich.edu>
2024-10-24 10:21:48 -0400

    updated project assets

--- sync_google_drive.sh
+++ sync_google_drive.sh
@@ -6,7 +6,7 @@ SRC="/Users/ruxinxie/Library/CloudStorage/GoogleDrive-ruxinx@umich.edu/My Drive/
 # Destination folder (local folder)
 DEST="/Users/ruxinxie/Website/portfolio-website/public/projects"
 
-# Use rsync to sync the contents (not the folder itself) to the 'projects' folder
-rsync -avz --delete "$SRC" "$DEST/"
+# Use rsync to sync the contents (not the folder itself) to the 'projects' folder, excluding 'not selected'
+rsync -avz --delete --exclude 'not selected' "$SRC" "$DEST/"
 
-echo "Sync complete! Content copied directly into 'projects'."
+echo "Sync complete! 'not selected' folder has been excluded."
